7b040e0bfc961b85df45423c38de74e368b653aa
Aijá Gourgel <agourgel44@gmail.com>
2026-06-18 06:58:08 +0100

    Criando o projecto

---
+++ src/main/java/com/prova/gerador_provas/GeradorProvasApplication.java
@@ -0,0 +1,13 @@
+package com.prova.gerador_provas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorProvasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorProvasApplication.class, args);
+	}
+
+}
